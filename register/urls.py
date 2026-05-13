f1001c470ef2d99a0392aa6e63e6df43faaa295b
leandroitec <l.odetto@itecriocuarto.org.ar>
2026-05-12 21:41:02 -0300

    listo el crud

--- register/urls.py
+++ register/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.registros, name='registros'),
     path('cargar-mascota/', views.cargar_mascota, name='cargar_mascota'),
+    path('registros/', views.registros, name='registros'),
+    path('editar/<int:id_mascota>/', views.editar_mascota, name='editar_mascota'),
+    path('eliminar/<int:id_mascota>/', views.eliminar_mascota, name='eliminar_mascota'),
                 ]
 
--- register/views.py
+++ register/views.py
@@ -1,22 +1,41 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Registro
 from .forms import MascotaForm
 
 def registros(request):
     registros_db = Registro.objects.all() 
-    return render(request, "appvete/register.html", {"registros": registros_db})
-    
-
-
+    return render(request, "register/registros.html", {"registros": registros_db})
 
 def cargar_mascota(request):
     if request.method == 'POST':
         form = MascotaForm(request.POST)
         if form.is_valid():
             form.save() 
-            return redirect('admin:index')
+            return redirect('registros')
     else:
         form = MascotaForm()
     
     return render(request, 'register/formulario_mascota.html', {'form': form})
+
+def editar_mascota(request, id_mascota):
+    mascota = get_object_or_404(Registro, id=id_mascota)
+    
+    if request.method == 'POST':
+        form = MascotaForm(request.POST, instance=mascota)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('registros') 
+    else:
+        form = MascotaForm(instance=mascota)
+    
+    return render(request, 'register/formulario_mascota.html', {'form': form, 'editando': True})
+
+def eliminar_mascota(request, id_mascota):
+    mascota = get_object_or_404(Registro, id=id_mascota)
+    
+    if request.method == 'POST':
+        mascota.delete() 
+        return redirect('registros') 
+        
+    return render(request, 'register/confirmar_eliminar.html', {'mascota': mascota})
